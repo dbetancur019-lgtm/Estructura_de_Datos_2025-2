827f6c0e500e5931e3a8a1fd70c8e34559cb0361
DiegoB <dbetancur019@gmail.com>
2025-09-25 18:47:53 -0500

    Se agrega el inicio del juego en main() con mensajes de bienvenida y posible primera jugada de la PC y se utiliza la funcion rand().

--- taller_1/triquitraque.cpp
+++ taller_1/triquitraque.cpp
@@ -5,3 +5,20 @@ using namespace std;
 
 char t[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 bool turno;
+
+int main() {
+    srand(time(0));
+    cout <<"Bienvenido a Triqui\n";
+    cout <<"Eres la X\n";
+    turno =rand() %2;
+    if(turno) {
+        cout <<"Empiezas tu\n";
+    } else {
+        cout <<"Empieza la computadora\n";
+    }
+    cout <<"Así están organizadas las posiciones:\n";
+    if(!turno) {
+        jugadaPC();
+        turno = true;
+    }
+}
